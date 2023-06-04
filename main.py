cf5663200ce41f6d34418bd7fd19ef013f118f3c
Edmond <yuenho092590@gmail.com>
2023-06-04 05:27:12 +0000

    add webCrawl

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.responses import HTMLResponse
+from service.webCrawl import weather_bug
 import uvicorn
 from pydantic import BaseModel
 from service.sql import CloudSqlConnector
@@ -24,7 +25,9 @@ app = FastAPI(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 SqlApi = CloudSqlConnector()
+WeatherCrawl = weather_bug('臺中市')
 
 def toDict(item: Item) -> dict:
     return {'temperature': item.temperature, 'moisture': item.moisture, 'soil_moisture': item.soil_moisture}
@@ -37,28 +40,58 @@ def main():
 @app.get('/execute/{stmt}')
 def exec(stmt: str):
     SqlApi.executeQuery(stmt)
+    return 'success'
 
 @app.get('/getData')
 def getData():
     return SqlApi.fetchData('plants')
 
-@app.get('/dataCheck', response_class=HTMLResponse)
+@app.get('/DataCheck/plants', response_class=HTMLResponse)
 def dataCheck(request: Request):
     
-    sent = '''| 溫度 | 濕度 | 土壤濕度 | 水重 | 時間 |\n| -- | -- | ---- | -- | -- |\n'''
+    sent = '''| 編號 | 溫度 | 濕度 | 土壤濕度 | 水重 | 時間 |\n| -- | -- | -- | ---- | -- | -- |\n'''
     
     result = SqlApi.fetchData('plants')
 
-    for i in result:
+    for n, i in enumerate(result):
         
         for j in range(len(i)) :
             i[j] = str(i[j])
             
-        sent += '| ' + ' | '.join(i) + ' |\n'
+        sent += f'| {str(n+1)} | ' + ' | '.join(i) + ' |\n'
         
      
     return templates.TemplateResponse("index.html",{'request':request, 'data' : markdown.markdown(sent, extensions=['markdown.extensions.tables'])})
 
+@app.get('/DataCheck/weather', response_class=HTMLResponse)
+def dataCheck(request: Request):
+    
+    sent = '''| 地區 | 預測種類 | 開始時間 | 結束時間 | 機率 |\n| -- | -- | -- | ---- | -- |\n| '''
+    
+    result = WeatherCrawl()
+
+    for v in result.values():
+            
+        sent += f'{v} | '
+        
+    return templates.TemplateResponse("index.html",{'request':request, 'data' : markdown.markdown(sent, extensions=['markdown.extensions.tables'])})
+
+@app.get('/DataCheck/watering', response_class=HTMLResponse)
+def dataCheck(request: Request):
+    
+    sent = '''| 時間 | 澆水量 |\n| -- | -- |\n'''
+    
+    result = SqlApi.fetchData('records')
+    
+
+    for i in result:
+        
+        i[0] = str(i[0])
+        i[1] = str(i[1])
+            
+        sent += '| ' + ' | '.join(i) + ' |\n'
+        
+    return templates.TemplateResponse("index.html",{'request':request, 'data' : markdown.markdown(sent, extensions=['markdown.extensions.tables'])})
 
 @app.post('/data')
 def data(item: Item):
@@ -80,9 +113,6 @@ def getFormattedData():
     formattedData = []
     data = SqlApi.fetchData('plants')
     
-    if len(data) > 7:
-        data = data[-7:]
-    
     for row in data:
         formattedData.append({'temperature': row[0], 'moisture': row[1], 'soil_moisture': row[2], 'gravity': row[3], 'time': row[4]})
     
@@ -91,20 +121,34 @@ def getFormattedData():
 @app.get('/getAppData')
 def getAppData():
     
-    appData = {'temperature': [], 'moisture': [], 'soil_moisture': [], 'gravity': [], 'time': []}
-    data = SqlApi.fetchData('plants')
+    threshold = 10
+    data = getFormattedData()
     
-    if len(data) > 7:
-        data = data[-7:]
-    
-    for row in data:
-        appData['temperature'].append(row[0])
-        appData['moisture'].append(row[1])
-        appData['soil_moisture'].append(row[2])
-        appData['gravity'].append(row[3])
-        appData['time'].append(row[4])
+    if len(data) > threshold:
+        data = data[-threshold:]
         
-    return appData
+    data = {'plant': data}
+    
+    data.update({'weather': WeatherCrawl()})
+    data.update({'watering': getWateringData()})
+    
+    return data
+
+
+def getWateringData():
+    
+    threshold = 3
+    data = SqlApi.fetchData('records')
+    
+    result = []
+    
+    if len(data) > threshold:
+        data = data[-threshold:]
+    
+    for i in data:
+        result.append({'time': i[0], 'water': i[1]})
+        
+    return result
 
 @app.exception_handler(StarletteHTTPException)
 async def custom_http_exception_handler(request, exc):
